49de5bd65b036d61ab3ec7027bf1f57bb8ddc12f
dmglynn <dmglynn@ncsu.edu>
2021-10-02 20:04:05 -0400

    added some basic laser functionality

---
+++ Puzzle Platformer/src/level_mechanics/Laser.gd
@@ -0,0 +1,44 @@
+# This program uses code from the "Make a Laser Beam in Godot in 1 Minute video by GDQuest.
+# CC-By 4.0 - GDQuest and contributors - https://www.gdquest.com/
+extends RayCast2D
+
+export var is_casting := true setget set_is_casting
+
+onready var collider = $"Area2D/Collider"
+
+
+func _ready():
+	set_physics_process(true)
+	$Line2D.points[1] = Vector2.ZERO
+
+
+func _physics_process(delta):
+	var cast_point := cast_to
+	force_raycast_update()
+	
+	if is_colliding():
+		cast_point = to_local(get_collision_point())
+	
+	$Line2D.points[1] = cast_point
+
+
+func set_is_casting(cast: bool):
+	is_casting = cast
+	
+	if is_casting:
+		appear()
+	else:
+		disappear()
+	set_physics_process(is_casting)
+
+
+func appear():
+	$Tween.stop_all()
+	$Tween.interpolate_property($Line2D, "width", 0, 10.0, 0.2)
+	$Tween.start()
+
+
+func disappear():
+	$Tween.stop_all()
+	$Tween.interpolate_property($Line2D, "width", 10.0, 0, 0.1)
+	$Tween.start()
