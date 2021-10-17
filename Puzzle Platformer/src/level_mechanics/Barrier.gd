62ad9352beb4a4c48a51d02a9a600dd0f6973ff6
dmglynn <dmglynn@ncsu.edu>
2021-10-16 23:23:37 -0400

    lever 3 added

--- Puzzle Platformer/src/level_mechanics/Barrier.gd
+++ Puzzle Platformer/src/level_mechanics/Barrier.gd
@@ -3,9 +3,19 @@ extends TileMap
 
 # bool value to indicate if the barrier is enabled or disabled
 var enabled = false
-var gone = false
+var locked = false
 
 onready var flip_sound = $FlipSound
+
+
+func _ready():
+	for tile in get_used_cells():
+		if get_cellv(tile) == 0:
+			enabled = false
+		else:
+			enabled = true
+		break
+
 
 func update_tilemap(id):
 	flip_sound.play()
@@ -14,30 +24,34 @@ func update_tilemap(id):
 	update_bitmask_region()
 
 
-# if lever signal is "enabled", calls _ready function again to check
-func _on_Lever_enable():
-	enabled = false
-	update_tilemap(0)
-
-
-func _on_Lever_disable():
-	enabled = true
-	update_tilemap(1)
-
-
 func _on_LaserSensor_enable():
 	enabled = false
-	gone = true
+	locked = true
 	update_tilemap(0)
 
 
 func _on_LaserReceiver_enable():
-	if !gone:
+	if !locked:
 		enabled = false
 		update_tilemap(0)
 
 
 func _on_LaserReceiver_disable():
-	if !gone:
+	if !locked:
 		enabled = true
 		update_tilemap(1)
+
+
+func _on_Lever_flip():
+	if !enabled:
+		enabled = true
+		update_tilemap(1)
+	else:
+		enabled = false
+		update_tilemap(0)
+
+
+func _on_LaserSensor_disable():
+	enabled = true
+	locked = true
+	update_tilemap(1)
