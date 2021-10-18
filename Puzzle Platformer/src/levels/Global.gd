c2130c670dc23b179a65dd4efe8c0eb84db69338
dmglynn <dmglynn@ncsu.edu>
2021-10-18 19:02:56 -0400

    title screen in progress

--- Puzzle Platformer/src/levels/Global.gd
+++ Puzzle Platformer/src/levels/Global.gd
@@ -1,7 +1,10 @@
 extends Node
 
-var current_scene = 1
+var current_scene = 0
 
 func go_next_stage(scene = current_scene + 1):
+	if (scene > 3):
+		print("wow! you win! pog")
+		return
 	current_scene = scene
 	get_tree().change_scene("res://src/levels/Level"+str(current_scene)+".tscn")
--- Puzzle Platformer/src/levels/TitleScreen.gd
+++ Puzzle Platformer/src/levels/TitleScreen.gd
@@ -1,16 +1,21 @@
 extends Node2D
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
+	$AnimationPlayer.play("title")
+
+
+func _on_Button_pressed():
+	Global.go_next_stage()
+
+
+func _on_LevelSelect_pressed():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _on_Settings_pressed():
+	pass # Replace with function body.
+
+
+func _on_Quit_pressed():
+	get_tree().quit()
