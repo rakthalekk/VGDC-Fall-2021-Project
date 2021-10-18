b527b860097671f2ac8c2fd37a40e2ac5c17b264
dmglynn <dmglynn@ncsu.edu>
2021-10-18 19:14:33 -0400

    buttons work (except settings)

--- Puzzle Platformer/src/levels/TitleScreen.gd
+++ Puzzle Platformer/src/levels/TitleScreen.gd
@@ -1,6 +1,9 @@
 extends Node2D
 
 
+onready var titlescrn = $TitleScreenButtons
+onready var lvlselect = $LevelSelectButtons
+
 func _ready():
 	$AnimationPlayer.play("title")
 
@@ -10,12 +13,30 @@ func _on_Button_pressed():
 
 
 func _on_LevelSelect_pressed():
-	pass # Replace with function body.
+	titlescrn.visible = false
+	lvlselect.visible = true
 
 
 func _on_Settings_pressed():
-	pass # Replace with function body.
+	print("sorry no settings yet")
 
 
 func _on_Quit_pressed():
 	get_tree().quit()
+
+
+func _on_Back_pressed():
+	lvlselect.visible = false
+	titlescrn.visible = true
+
+
+func _on_Lvl1_pressed():
+	Global.go_next_stage(1)
+
+
+func _on_Lvl2_pressed():
+	Global.go_next_stage(2)
+
+
+func _on_Lvl3_pressed():
+	Global.go_next_stage(3)
