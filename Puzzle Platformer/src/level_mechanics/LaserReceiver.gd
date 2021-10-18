0ee3b770fee620c96db0604a70ef316e6ebef55d
dmglynn <dmglynn@ncsu.edu>
2021-10-18 18:24:13 -0400

    push

--- Puzzle Platformer/src/level_mechanics/LaserReceiver.gd
+++ Puzzle Platformer/src/level_mechanics/LaserReceiver.gd
@@ -5,15 +5,16 @@ signal disable
 
 var counter = 0
 
-func _process(delta):
-	if counter > 0:
-		counter -= 1
-	elif powered:
+onready var timer = $Timer
+
+func power_on():
+	timer.start()
+	if !powered:
+		anim_player.play("charging")
+
+
+func _on_Timer_timeout():
+	if powered:
 		powered = false
 		emit_signal("disable")
 		anim_player.play("empty")
-
-func power_on():
-	counter = 30
-	if !powered:
-		anim_player.play("charging")
